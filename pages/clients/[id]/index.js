8acd3040be9f43027d63d908ac1d7652d7e8eef3
Utsav Karan <ukaran11@gmail.com>
2021-10-16 14:55:28 +0530

    Utsav | Added programmatic navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ function ClientProjectsPage() {
     const router = useRouter();
 
     console.log(router.query);
+
+    const loadProjectHandler = () => {
+        // load data
+        // router.push('/clients/max/projectA')
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id:'max', clientprojectid:'projectA'}
+        })
+    }
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
