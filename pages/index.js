f15e3c4374ff287fb0a9c6ef9f0f3fb94bab6a0e
Utsav Karan <ukaran11@gmail.com>
2021-10-16 14:34:20 +0530

    Utsav | Added Navigation to Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ function HomePage() {
         <li>
           <Link href="/portfolio">Portfolio</Link>
         </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
       </ul>
     </div>
   )
